4d6795c3f6781516fbf565dd664ea695dc599fa6
Victor <victor.augusto@qitech.com.br>
2024-05-15 02:34:02 -0300

    feat: database migration

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,38 +1,27 @@
 import sqlite3
 
-DATABASE_URL = './database.db'
+connection = sqlite3.connect('database.db')
+cursor = connection.cursor()
 
-def get_product_information():
-    connection = sqlite3.connect(DATABASE_URL)
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM products")
-    products = cursor.fetchall()
-    connection.close()
-    return products
+# Criando tabela de produtos
+cursor.execute('''
+CREATE TABLE products (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL
+)
+''')
 
+# Inserindo dados de exemplo
+products = [
+    (1, 'Produto A', 'Descrição do Produto A...'),
+    (2, 'Produto B', 'Descrição do Produto B...'),
+    (3, 'Produto C', 'Descrição do Produto C...'),
+]
 
-def initialize_database():
-    connection = sqlite3.connect(DATABASE_URL)
-    cursor = connection.cursor()
+cursor.executemany('''
+INSERT INTO products (id, name, description) VALUES (?, ?, ?)
+''', products)
 
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS products (
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        description TEXT NOT NULL
-    )
-    ''')
-
-    products = [
-        (1, 'Produto A', 'Descrição do Produto A...'),
-        (2, 'Produto B', 'Descrição do Produto B...'),
-        (3, 'Produto C', 'Descrição do Produto C...'),
-    ]
-
-    cursor.executemany('''
-    INSERT INTO products (id, name, description)
-    VALUES (?, ?, ?)
-    ''', products)
-
-    connection.commit()
-    connection.close()
+connection.commit()
+connection.close()
