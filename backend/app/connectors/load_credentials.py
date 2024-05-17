9e632a9e3cbcb66577d2bec75b1b7adbf778bfbc
Victor <victor.augusto@qitech.com.br>
2024-05-17 14:06:12 -0300

    feat: load persistent credentials

--- backend/app/connectors/load_credentials.py
+++ backend/app/connectors/load_credentials.py
@@ -18,15 +18,18 @@ def load_creds():
     # time.
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    try:
     # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'app/connectors/client_secret.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'app/connectors/client_secret.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+    except Exception as e:
+        print(e)
     return creds
